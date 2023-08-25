8907fc8ed8e066b2841d527f84a9999fd52c93df
Teckos <teckos@hotmail.fr>
2023-08-25 11:01:38 +0200

    Initial commit

---
+++ src/main/java/fr/eni/tppanier/TpPanierApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.tppanier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPanierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpPanierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/eni/tppanier/TpPanierApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.tppanier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPanierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
